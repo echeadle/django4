2f22fe14e652ceb2ee63b1b29d090e5d128c28a5
echeadle <echeadle@gmail.com>
2022-07-16 10:25:22 -0600

    add url and view for products

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+def products(request):
+    return HttpResponse("List of products")

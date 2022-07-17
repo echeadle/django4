8bdb7850ccf1cd6c5413a7b2a1b7c15c4bdc668e
echeadle <echeadle@gmail.com>
2022-07-17 11:35:51 -0600

    Add namespace to myapp/urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'myapp'
+
 urlpatterns = [
     path('',views.index),
     path('products/', views.products),

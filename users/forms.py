5cf5751c0b3acb46bb69c4c9bd5cd5dc68118beb
echeadle <echeadle@gmail.com>
2022-07-22 16:32:12 -0600

    Section 70, complete register functionality

--- users/forms.py
+++ users/forms.py
@@ -11,4 +11,10 @@ class NewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
-        
+    
+    def save(self,commit=True):
+        user = super(NewUserForm,self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user

9866c6e9bbc9185c4c73d8472c9a57ab42ed9e09
echeadle <echeadle@gmail.com>
2022-07-22 19:22:39 -0600

    Section 72 style login page

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as authentication_views
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/',views.register,name='register'),
+    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login')
 ]

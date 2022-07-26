1629ecacae6696335cb34330c448deb840b1a9f5
echeadle <echeadle@gmail.com>
2022-07-25 23:16:47 -0600

    Section 99 modify Admin web page headers

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,3 +3,6 @@ from .models import Product
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.site_header = "Buy & Sell Website"
+admin.site.site_title  = "ABC Buying"
+admin.site.index_title = "Manage ABC Buying Website"

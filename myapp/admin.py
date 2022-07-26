514db79d7c13c2f6fd71159a03a95fe8b419e2ba
echeadle <echeadle@gmail.com>
2022-07-25 23:25:31 -0600

    Section 100 add custom fields

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+
 admin.site.site_header = "Buy & Sell Website"
 admin.site.site_title  = "ABC Buying"
 admin.site.index_title = "Manage ABC Buying Website"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display= ('name','price','desc')
+
+admin.site.register(Product,ProductAdmin)
+
+
